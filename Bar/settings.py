a452158c1d5f708d04bff8fe54dc34e6ead9c20e
crisvac10 <82240074+crisvac10@users.noreply.github.com>
2024-05-25 09:02:34 -0500

    Update settings.py

--- Bar/settings.py
+++ Bar/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.environ.get('SECRETE_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = [   ]
+ALLOWED_HOSTS = ['*'
+]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
